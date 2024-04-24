997a65cb033bf68706b32cb0f518fd781d1ba444
Imrane Arabah <59772524+arabahimrane@users.noreply.github.com>
2024-04-24 14:14:13 -0400

    fix

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-""" for a given employee ID, returns information about his/her TODO list """
-
-from requests import get
+"""Exports to-do list information for a given employee ID to JSON format."""
 import json
+import requests
 import sys
 
-
-def get_employee_progress(em_id):
-    """ func to get data from api """
-    emp_infos = get('https://jsonplaceholder.typicode.com/users/{}/'
-                    .format(em_id)).json()
-    emp_progress = get('https://jsonplaceholder.typicode.com/users/{}/todos'
-                       .format(em_id)).json()
-    data = {str(em_id): []}
-    for it in emp_progress:
-        data[str(em_id)].append({
-            "task": it['title'],
-            "completed": it['completed'],
-            "username": emp_infos['username']
-            })
-
-    with open('{}.json'.format(sys.argv[1]), 'w') as f:
-        f.write(json.dumps(data))
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
-    else:
-        employee_id = int(sys.argv[1])
-        get_employee_progress(employee_id)
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
